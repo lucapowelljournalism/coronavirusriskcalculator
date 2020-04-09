c6e4075f4ba35d1c19848e0bf2d01393dde285db
JR Powell <jrpowell@FSPFVFZ929ML414.local>
2020-04-09 17:56:18 -0400

    made it look a little easier on the eye

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # coronavirusriskcalculator
+
+This interactive should help a user personalize coronavirus in terms of their own health, location, and anything else that seems relevant.
+
+1) Have a nice clean CSS.
+2) Input at least 4-5 things from the user.
+3) Output interesting info based on those inputs.
+  3a)Which means we need datasets
+    data 1) Covid Death case rate
+    data 2) Preexisting condition case rate
+    ??
+    ??
+    ??
+4) What else can we do?
--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+const data = [{ "age_group_low": 0, "age_group_high": 17, "COVID_CASE_RATE": 47.25, "HOSPITALIZED_CASE_RATE": 4.81, "DEATH_RATE": 0.06 },
+	{ "age_group_low": 18, "age_group_high": 44, "COVID_CASE_RATE": 556.94, "HOSPITALIZED_CASE_RATE": 50.78, "DEATH_RATE": 2.31 },
+	{ "age_group_low": 45, "age_group_high": 64, "COVID_CASE_RATE": 783.25, "HOSPITALIZED_CASE_RATE": 177.91, "DEATH_RATE": 16.2 },
+	{ "age_group_low": 65, "age_group_high": 75, "COVID_CASE_RATE": 798.72, "HOSPITALIZED_CASE_RATE": 302.38, "DEATH_RATE": 47.77 },
+	{ "age_group_low": 75, "age_group_high": 1000, "COVID_CASE_RATE": 791.26, "HOSPITALIZED_CASE_RATE": 403.86, "DEATH_RATE": 114.81 }];
+
 $("body").on("keyup keydown keypress change", ".calculator input", function (e) {
 	let user_input = $(this).val() //Stores input
+	let age = parseInt(user_input);
 
+	let group = data.find(function (row) {
+		if (row.age_group_low <= age && row.age_group_high >= age) {
+			return true;
+		}
+	});
 
+	const case_rate = group.DEATH_RATE;
+	document.querySelector("#caserate").innerHTML = case_rate
 });
