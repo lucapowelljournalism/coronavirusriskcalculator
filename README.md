2209d684639b8e939cf58cc086f707fb9f8f117a
JR Powell <jrpowell@FSPFVFZ929ML414.local>
2020-04-21 16:12:05 -0400

    added click-reveal functionality to each question

--- README.md
+++ README.md
@@ -7,8 +7,11 @@ This interactive should help a user personalize coronavirus in terms of their ow
 3) Output interesting info based on those inputs.
   3a)Which means we need datasets
     data 1) Covid Death case rate
-    data 2) Preexisting condition case rate
+    data 2) Preexisting condition case rate (We need to get data that says, ok if you have asthma, the death percentage is this)
     ??
     ??
     ??
 4) What else can we do?
+
+-   race/ethnicity/background of some kind? I know if you live in NYC its 2xhigher death rate if black/brown.
+-   zipcode?
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Data for age-based death rate//
 const data = [{ "age_group_low": 0, "age_group_high": 17, "COVID_CASE_RATE": 47.25, "HOSPITALIZED_CASE_RATE": 4.81, "DEATH_RATE": 0.06 },
 	{ "age_group_low": 18, "age_group_high": 44, "COVID_CASE_RATE": 556.94, "HOSPITALIZED_CASE_RATE": 50.78, "DEATH_RATE": 2.31 },
 	{ "age_group_low": 45, "age_group_high": 64, "COVID_CASE_RATE": 783.25, "HOSPITALIZED_CASE_RATE": 177.91, "DEATH_RATE": 16.2 },
@@ -8,6 +9,7 @@ $("body").on("keyup keydown keypress change", ".calculator input", function (e) 
 	let user_input = $(this).val() //Stores input
 	let age = parseInt(user_input);
 
+	//Finds the row that corresponds to the age range
 	let group = data.find(function (row) {
 		if (row.age_group_low <= age && row.age_group_high >= age) {
 			return true;
@@ -16,4 +18,54 @@ $("body").on("keyup keydown keypress change", ".calculator input", function (e) 
 
 	const case_rate = group.DEATH_RATE;
 	document.querySelector("#caserate").innerHTML = case_rate
+	//need to add class active to the output text that accompanies this//
+});
+
+//Make the sex tile active//
+$("body").on("click", ".beginbutton", function (e) {
+	e.preventDefault();
+	console.log("clicking works")
+	$(".sex").removeClass('inactive')
+});
+
+//Make the location tile active//
+$("body").on("click", ".sex .tiles", function (e) {
+	e.preventDefault();
+	console.log("clicking works")
+	$(".location").removeClass('inactive')
+});
+
+//Make the age tile active//
+$("body").on("click", ".location", function (e) {
+	e.preventDefault();
+	console.log("clicking works")
+	$(".age").removeClass('inactive')
+});
+
+//Make the ethnicity tile active//
+$("body").on("click", ".age", function (e) {
+	e.preventDefault();
+	console.log("clicking works")
+	$(".ethnicity").removeClass('inactive')
+});
+
+//Make the conditions tile active//
+$("body").on("click", ".ethnicity", function (e) {
+	e.preventDefault();
+	console.log("clicking works")
+	$(".conditions").removeClass('inactive')
+});
+
+//Make the bloodtype tile active//
+$("body").on("click", ".conditions", function (e) {
+	e.preventDefault();
+	console.log("clicking works")
+	$(".bloodtype").removeClass('inactive')
+});
+
+//Make the location tile active//
+$("body").on("click", ".bloodtype", function (e) {
+	e.preventDefault();
+	console.log("clicking works")
+	$(".diseases").removeClass('inactive')
 });
