1aaf769eb59a49407c0d6d0cd0ff6a04d594b7e1
JR Powell <jrpowell@FSPFVFZ929ML414.local>
2020-04-21 20:13:33 -0400

    changess from April 21st Class

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
-//Data for age-based death rate//
-const data = [{ "age_group_low": 0, "age_group_high": 17, "COVID_CASE_RATE": 47.25, "HOSPITALIZED_CASE_RATE": 4.81, "DEATH_RATE": 0.06 },
-	{ "age_group_low": 18, "age_group_high": 44, "COVID_CASE_RATE": 556.94, "HOSPITALIZED_CASE_RATE": 50.78, "DEATH_RATE": 2.31 },
-	{ "age_group_low": 45, "age_group_high": 64, "COVID_CASE_RATE": 783.25, "HOSPITALIZED_CASE_RATE": 177.91, "DEATH_RATE": 16.2 },
-	{ "age_group_low": 65, "age_group_high": 75, "COVID_CASE_RATE": 798.72, "HOSPITALIZED_CASE_RATE": 302.38, "DEATH_RATE": 47.77 },
-	{ "age_group_low": 75, "age_group_high": 1000, "COVID_CASE_RATE": 791.26, "HOSPITALIZED_CASE_RATE": 403.86, "DEATH_RATE": 114.81 }];
-
-$("body").on("keyup keydown keypress change", ".calculator input", function (e) {
-	let user_input = $(this).val() //Stores input
-	let age = parseInt(user_input);
-
-	//Finds the row that corresponds to the age range
-	let group = data.find(function (row) {
-		if (row.age_group_low <= age && row.age_group_high >= age) {
-			return true;
-		}
-	});
-
-	const case_rate = group.DEATH_RATE;
-	document.querySelector("#caserate").innerHTML = case_rate
-	//need to add class active to the output text that accompanies this//
-});
-
 //Make the sex tile active//
 $("body").on("click", ".beginbutton", function (e) {
 	e.preventDefault();
@@ -68,4 +45,74 @@ $("body").on("click", ".bloodtype", function (e) {
 	e.preventDefault();
 	console.log("clicking works")
 	$(".diseases").removeClass('inactive')
+});
+
+
+//DATA FOR PREEXISTING CONDITIONS//
+
+//DATA FOR STATES//
+
+//DATA FOR ETHNICITY//
+
+
+//Data for sex//
+const sexdata = [{ "sexgroup": 'Female', "COVID_CASE_RATE": 913.66, "HOSPITALIZED_CASE_RATE": 196.19, "DEATH_RATE": 39.25 },
+	{ "sexgroup": 'Male', "COVID_CASE_RATE": 1178.07, "HOSPITALIZED_CASE_RATE": 318.75, "DEATH_RATE": 71.09 }];
+
+$("body").on("click", ".sex", function (e) {
+	e.preventDefault();
+
+	let click_sex = e.target; //
+	let target = $(click_sex).data("target"); // "gets data attribute #(variablename) from click face."
+	console.log(target)
+
+	//NOT FINISHED YET!!//
+
+});
+
+
+
+
+//Data for bloodtype rate//
+const bloodtypedata = [{ "bloodtype_group": 'A', "COVID_CASE_RATE": 10.03 },
+	{ "bloodtype_group": 'B', "COVID_CASE_RATE": 26.42 },
+	{ "bloodtype_group": 'O', "COVID_CASE_RATE": 25.80 },
+	{ "bloodtype_group": 'AB', "COVID_CASE_RATE": 10.03 }];
+
+$("body").on("keyup keydown keypress change", ".calculator.bloodtype select", function (e) {
+	let type = $(this).val() //Stores input
+	//Finds the row that corresponds to the age range
+	let group = bloodtypedata.find(function (row) {
+		if (row.bloodtype_group === type) {
+			return true;
+		}
+	});
+
+	const bloodtype_case_rate = group.COVID_CASE_RATE;
+	document.querySelector("#susceptibility").innerHTML = bloodtype_case_rate;
+	console.log(case_rate);
+});
+
+
+//Data for age-based death rate//
+const data = [{ "age_group_low": 0, "age_group_high": 17, "COVID_CASE_RATE": 47.25, "HOSPITALIZED_CASE_RATE": 4.81, "DEATH_RATE": 0.06 },
+	{ "age_group_low": 18, "age_group_high": 44, "COVID_CASE_RATE": 556.94, "HOSPITALIZED_CASE_RATE": 50.78, "DEATH_RATE": 2.31 },
+	{ "age_group_low": 45, "age_group_high": 64, "COVID_CASE_RATE": 783.25, "HOSPITALIZED_CASE_RATE": 177.91, "DEATH_RATE": 16.2 },
+	{ "age_group_low": 65, "age_group_high": 75, "COVID_CASE_RATE": 798.72, "HOSPITALIZED_CASE_RATE": 302.38, "DEATH_RATE": 47.77 },
+	{ "age_group_low": 75, "age_group_high": 1000, "COVID_CASE_RATE": 791.26, "HOSPITALIZED_CASE_RATE": 403.86, "DEATH_RATE": 114.81 }];
+
+$("body").on("keyup keydown keypress change", ".calculator.age input", function (e) {
+	let user_input = $(this).val(); //Stores input
+	let age = parseInt(user_input);
+
+	//Finds the row that corresponds to the age range
+	let group = data.find(function (row) {
+		if (row.age_group_low <= age && row.age_group_high >= age) {
+			return true;
+		}
+	});
+
+	const age_case_rate = group.DEATH_RATE;
+	document.querySelector("#caserate").innerHTML = age_case_rate
+	//need to add class active to the output text that accompanies this//
 });
